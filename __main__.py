9e76bfa625d523c7f719f6918058a69ee1ec2694
Juan Reséndiz <jresendiz27@gmail.com>
2025-07-08 16:45:40 -0600

    Create scaffolding for ms_pacman rl project

---
+++ __main__.py
@@ -0,0 +1,19 @@
+from model.agent import Agent
+
+
+def run_agent():
+    pacman_agent = Agent(
+        alpha=0,
+        gamma=0.9,
+        epsilon=0.1,
+        n_actions=10,
+        input_dims=(80,80,3),
+        mem_size=2000,
+        epsilon_end=0.001,
+        batch_size=100,
+    )
+
+
+
+if __name__ == '__main__':
+    print("Main")
